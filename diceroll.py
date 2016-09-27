56a3f555a8612a3e1a9382976c58e2426fca2f94
RubyLinux <linuxlucifer@gmail.com>
2016-09-27 12:56:46 +0100

    Update diceroll.py

--- diceroll.py
+++ diceroll.py
@@ -1,10 +1,6 @@
 import random
 min = 1
 max = 6
-
-roll = "yes"
-
-while roll == "yes":
-    print "The numbers on the dice are:"
-    print random.randint(min, max)
-    print random.randint(min, max)
+print "The numbers on the dice are:"
+print random.randint(min, max)
+print random.randint(min, max)
